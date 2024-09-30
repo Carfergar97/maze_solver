8e0d348b0b46de6b2706f8a02caf9d023b8de450
Carfergar97 <cfdezg1997@gmail.com>
2024-09-30 09:07:49 +0200

    Moved the Maze class to a new maze.py module

--- src/window.py
+++ src/window.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
-from graphics import Point, Line, Cell, Maze
+from graphics import Point, Line, Cell
+from maze import Maze
 class Window():
     def __init__(self, widht:int, height:int) -> None:
 

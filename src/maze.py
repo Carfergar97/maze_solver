db7a821e75305a65e8bd109f96de2434a7446d1a
Carfergar97 <cfdezg1997@gmail.com>
2024-09-30 13:48:49 +0200

    Added the break walls recursive maze method

--- src/maze.py
+++ src/maze.py
@@ -1,9 +1,9 @@
 import time
 from graphics import Cell
-
+import random
 class Maze():
 
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed = None):
         self._cells = [ ]
         self._x1 = x1
         self._y1 = y1
@@ -12,8 +12,11 @@ class Maze():
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if(seed != None):
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
 
     def _create_cells(self):
         
@@ -43,3 +46,43 @@ class Maze():
         self._draw_cell(0,0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+    
+    def _break_walls_r(self,i,j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            if i - 1 >= 0:
+                if self._cells[i-1][j].visited == False:
+                    to_visit.append((i-1,j))
+            if i + 1 < self._num_cols:
+                if self._cells[i+1][j].visited == False:
+                    to_visit.append((i+1,j))
+            if j - 1 >= 0:
+                if self._cells[i][j - 1].visited == False:
+                    to_visit.append((i,j - 1))
+            if j + 1 < self._num_rows:
+                if self._cells[i][j + 1].visited == False:
+                    to_visit.append((i,j + 1))
+            
+            if len(to_visit) == 0:
+                self._draw_cell(i,j)
+                return
+            
+            next = random.randrange(len(to_visit))
+
+            if i == to_visit[next][0]:
+                if j < to_visit[next][1]:
+                    self._cells[i][j].has_top_wall = False
+                    self._cells[i][to_visit[next][1]].has_bottom_wall = False
+                else:
+                    self._cells[i][j].has_bottom_wall = False
+                    self._cells[i][to_visit[next][1]].has_top_wall = False
+            else:
+                if i < to_visit[next][0]:
+                    self._cells[i][j].has_right_wall = False
+                    self._cells[to_visit[next][0]][j].has_left_wall = False
+                else:
+                    self._cells[i][j].has_left_wall = False
+                    self._cells[to_visit[next][0]][j].has_right_wall = False
+            
+            self._break_walls_r(to_visit[next][0],to_visit[next][1])
